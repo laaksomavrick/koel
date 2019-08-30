0caa625edfbd9e11bcebff9af75b426f8257ca24
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-08-29 23:33:45 -0400

    Stack works; lambda non-functional (code organization error; unable to import module main)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,21 @@ rm -rf ./build
 mkdir ./build
 pipenv lock -r > build/requirements.txt
 pipenv run pip download -d ./build/vendor -r ./build/requirements.txt
-zip -r build/lambda.zip builld/vendor
+zip -r build/lambda.zip build/vendor
 zip -gr build/lambda.zip koel
 zip -g build/lambda.zip main.py
+zip -g build/lambda.zip config.yaml
 
 # Create a bucket specifically for our lambda functions
-aws s3api put-object --bucket my-lambda-functions --key PullMarketCode --body ./build/lambda.zip
+aws s3api create-bucket --bucket=koel-lambda-code
 
 # Upload our lambda function to that bucket
+# TODO: paramaterize this with a version number
+aws s3api put-object --bucket koel-lambda-code --key koel:0.0.1 --body ./build/lambda.zip
 
 # Create our s3 bucket and lambda
- aws cloudformation create-stack --stack-name koel --template-body file://cloudformation.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack \
+                  --stack-name koel \
+                  --template-body file://cloudformation.yaml \
+                  --capabilities CAPABILITY_NAMED_IAM \
+                  --parameters ParameterKey=S3Bucket,ParameterValue=koel-lambda-code ParameterKey=S3Key,ParameterValue=koel:0.0.1
